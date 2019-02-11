e09bb77a925a3b967f7b3acd9dba9d1db0bac060
Nicholas COOK <nicholascook@iMac10.local>
2019-02-11 14:02:14 -0500

    Finished is else statements

---
+++ Lab - Control Flow.playground/Pages/1. Exercise - Logical Operators.xcplaygroundpage/Contents.swift
@@ -0,0 +1,52 @@
+/*:
+ ## Exercise - Logical Operators
+
+ For each of the logical expressions below, print out what you think the resulting value will be (`true` or `false`). Then print out the actual expression to see if you were right. An example has been provided below.
+ 
+    43 == 53
+    print(false)
+    print(43 == 53)
+
+ 
+ 1. `9 == 9`
+ */
+print(true)
+
+/*:
+ 2. `9 != 9`
+ */
+print(false)
+9==9
+/*: 
+ 3. `47 > 90`
+ */
+print(false)
+47 < 90
+/*:
+ 4. `47 < 90`
+ */
+print(true)
+
+/*:
+ 5. `4 <= 4`
+ */
+print(true)
+
+/*:
+ 6. `4 >= 5`
+ */
+print(false)
+4 < 5
+
+
+/*:
+ 8. `(47 > 90) || (47 < 90)`
+ */
+print(false)
+(47 < 90) || (47 < 90)
+/*:
+ 9. `!true`
+ */
+print(false)
+true = true
+//: page 1 of 9  |  [Next: Exercise - If and If-Else Statements](@next)
